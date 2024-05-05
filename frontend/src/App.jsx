41608a00b5271135b47f42ff2ff14179fb63ed43
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2024-05-05 20:23:09 +0530

    perf: remove unused code from app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,8 @@ import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import { useEffect, useState } from "react";
 
 function App() {
-  const currentPath = window.location.pathname; // change this with user login status
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    if (currentPath !== "/") {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
-    }
-  }, [currentPath]);
-
   return (
     <div className="min-h-[100vh] w-[100%]">
       <div className="pages">
